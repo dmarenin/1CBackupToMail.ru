97724b285199dd66a77b21baa9f3d8034089fa54
le9ra6 <mexanik_96@mail.ru>
2019-01-12 20:22:19 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using MailRuCloudApi;
+using System.IO;
+using System.ComponentModel;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static private Account account;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length.ToString());
+
+            if (args.Length != 4)
+            {
+                return;
+            }
+
+            var fileName = args[0];
+            var destinationPath = args[1];
+            var login = args[2];
+            var password = args[3];
+
+            account  = new Account(login, password);
+
+            var api = new MailRuCloud() { Account = account };
+
+            //var percent = 0;
+            api.ChangingProgressEvent += delegate (object sender, ProgressChangedEventArgs e)
+            {
+                //percent = e.ProgressPercentage
+                Console.WriteLine("uploading "+e.ProgressPercentage);
+            };
+
+            api.UploadFile(new FileInfo(fileName), destinationPath).Wait();
+
+            DateTime today = DateTime.Now;
+            DateTime answer = today.AddDays(Convert.ToDouble(-45));
+
+            Entry items = api.GetItems(destinationPath).Result;
+            foreach (MailRuCloudApi.File f in items.Files)
+            {
+                if (f.LastModifiedTimeUTC<answer)
+                {
+                    api.Remove(f.FulPath).Wait();
+                }
+            }
+
+            api = null;
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # 1CBackupToMail.ru
 sample mail web dav client
+
+this is clone repo from https://github.com/erastmorgan/Mail.Ru-.net-cloud-client
+
+https://infostart.ru/public/828998/
+
+for start:
+
+path_to_exe "path_unload_file" "path_cloud_folder" "login "password"
+
+sample:
+
+create bak
+
+"C:\Program Files\7-Zip\7z.exe" a "E:\R_BASE\komi\%date:~6,4%_%date:~3,2%_%date:~0,2%.7z" "E:\komi_retail" -ssw -mf=BCJ2 -mx
+"E:\copy to cloud\ConsoleApplication1.exe" "E:\R_BASE\komi\%date:~6,4%_%date:~3,2%_%date:~0,2%.7z" "/komi/"
+
+upload
+
+"E:\copy to cloud\ConsoleApplication1.exe" "E:\R_BASE\gs\%date:~6,4%_%date:~3,2%_%date:~0,2%.7z" "/gs/" "dev@mail.ru" "22215"
+
